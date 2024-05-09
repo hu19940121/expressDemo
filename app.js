a12f5e57466d5846698e77ede233f2d721aa9907
hushihao <673822024@qq.com>
2024-05-09 18:56:11 +0800

    feat: 更改内容

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(80, () => console.log("Server ready on port 80."));
-
+// app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <title>哈哈哈哈</title>
+      </head>
+      <body>
+        <h1>213213</h1>
+        <p>恭喜</p>
+      </body>
+    </html>
+  `);
+});
+// app.listen(80, () => console.log("Server ready on port 80."));
+app.listen(process.env.PORT || 3000);
 module.exports = app;
